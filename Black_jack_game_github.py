2856375beb2eb107118c8f71d31c52745391ff4a
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-01 21:37:07 -0800

    look at ### for comments may be key to fixing bug, also look at pass way user_card_count function as fixed

--- Black_jack_game_github.py
+++ Black_jack_game_github.py
@@ -121,7 +121,7 @@ def play_again(playing_deck):
     print ("Dealers count: " + dealer_total_count_hiddenstr)
     print ("   Your Cards:" + users_cards)
     return (user_card_counter(dealer_total_count_hiddenstr,user_cards_with_suits,dealers_cards, dealers_cards_hidden,playing_deck,users_cards))
-        
+    #user_card_counter() missing 1 required positional argument: 'users_cards'######
 
 #Variables for dealer count function
 dealer_cards_with_suits = [] 
